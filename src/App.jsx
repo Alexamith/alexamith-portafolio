602b99d5ce0a0a5806abd0d2c667eff80beb0d3b
Alex <alexcambronero18@gmail.com>
2025-05-06 16:47:09 -0600

    Adding initial loading

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import './index.css'
+import { LoadingScreen } from './componets/LoadingScreen'
+import { useState } from 'react'
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  return (
+    <>
+      {
+        !isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />
+
+      }
+      
+    </>
+  )
+}
+
+export default App
