8b698b3811312a67e8fdade51bfea251513c75b6
Alex <alexcambronero18@gmail.com>
2025-05-07 09:01:55 -0600

    Adding menu and responsive menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,26 @@ import './App.css'
 import './index.css'
 import { LoadingScreen } from './componets/LoadingScreen'
 import { useState } from 'react'
+import { Navbar } from './componets/Navbar'
+import { MobileMenu } from './componets/MobileMenu'
+import { Home } from './componets/sections/Home'
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
-      {
-        !isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+        <div className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}>
 
-      }
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <Home/>
+
+        </div>
       
     </>
   )
---
+++ src/componets/sections/Home.jsx
@@ -0,0 +1,22 @@
+
+export const Home = () => {
+  return (
+    <section id="home" className="min-h-screen flex items-center justify-center relative"> 
+        
+        <div className="text-center z-10 px-4">
+
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
+                Hi, I'm Alex
+            </h1>
+
+            <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+                Hey! I’m Alexamith, but just call me Alex. I’m a full stack developer with several years of experience building software. 
+                I love writing clean, scalable code, delivering high-quality products, and making sure clients are genuinely happy. 
+                My goal is to be better than I was yesterday and keep pushing myself with every project.
+            </p>
+
+        </div>
+
+    </section>
+  )
+}
