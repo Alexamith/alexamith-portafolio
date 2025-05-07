575ddbaa7bd188de8b64bc3211449eead38d80ae
Alex <alexcambronero18@gmail.com>
2025-05-07 09:17:51 -0600

    Adding home section

--- src/componets/sections/Home.jsx
+++ src/componets/sections/Home.jsx
@@ -14,6 +14,16 @@ export const Home = () => {
                 I love writing clean, scalable code, delivering high-quality products, and making sure clients are genuinely happy. 
                 My goal is to be better than I was yesterday and keep pushing myself with every project.
             </p>
+            <div className="flex justify-center space-x-4" >
+                <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden 
+                hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">
+                    View projects
+                </a>
+                <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
+                hover:-translate-y-0.5 hover:shadow-[0,0,15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10">
+                    Contact me
+                </a>
+            </div>
 
         </div>
 
